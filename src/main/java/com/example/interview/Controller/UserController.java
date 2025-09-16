bad48b1c7a2de218476830b6d824b8d6e80e3bef
Dame <lklkklkl@naver.com>
2025-09-16 17:09:46 +0900

    - http 인코딩 설정 - mvc 패턴 예제 코드 생성

--- src/main/java/com/example/interview/Controller/UserController.java
+++ src/main/java/com/example/interview/Controller/UserController.java
@@ -5,17 +5,20 @@ import com.example.interview.Service.UserServiceImpl;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 @Tag(name = "User API", description = "회원 관련 API")
 public class UserController {
 
     @Autowired
     private UserServiceImpl userServiceImpl;
 
-    @PostMapping()
-    private void save(UserVO userVO){
+    @PostMapping("/save")
+    private void save(@RequestBody UserVO userVO){
         userServiceImpl.save(userVO);
     }
 }
--- src/main/java/com/example/interview/Service/UserServiceImpl.java
+++ src/main/java/com/example/interview/Service/UserServiceImpl.java
@@ -9,5 +9,21 @@ public class UserServiceImpl implements UserService{
     @Override
     public void save(UserVO userVO){
 
+        try {
+            System.out.println("ID : " + userVO.getUserId());
+            System.out.println("PW : " + userVO.getUserPw());
+            System.out.println("Name : " + userVO.getUserName());
+            System.out.println("Age : " + userVO.getAge());
+            System.out.println("Gender : " + userVO.getGender());
+            System.out.println("Address : " + userVO.getAddress());
+            System.out.println("PhoneNum : " + userVO.getPhoneNum());
+
+            //TODO db 연동 및 쿼리 작성
+            //TODO try with resource 적용해보기
+
+        } catch(Exception e) {
+            e.getStackTrace();
+        }
+
     }
 }
